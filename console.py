f279474769771a024dbca5b04fa5000f3e0834b3
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-06 14:18:22 -0600

    update

---
+++ console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Console for the application. """
+import cmd
+
+import models
+from models import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
