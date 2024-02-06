46ff63b8ad046c538eb7b9b85862c79630f584f5
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-02-06 14:47:17 -0600

    adding on

--- console.py
+++ console.py
@@ -11,3 +11,24 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Class that contains the console for the application.
+    """
+
+    prompt = '(hbnb) '
+    __classes = {'BaseModel': BaseModel, 'User': User,
+                 'State': State, 'City': City, 'Amenity': Amenity,
+                 'Place': Place, 'Review': Review}
+
+    def do_create(self, *args):
+        """
+        Creates an instance of a class
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(args[0] + "." + str(eval(args[0] + "()")))
