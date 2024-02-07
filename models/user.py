05fddde236213ae0af86cf237301e2163ebea53f
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-07 16:44:57 -0600

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """class User with the inheritance from Base"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    '''
+        Definition of the User class
+    '''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
