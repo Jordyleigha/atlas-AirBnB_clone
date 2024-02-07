deb822da5d11c364d85a535091acdeefcff0a0c0
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-07 16:46:16 -0600

    Update __init__.py

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,8 +1,4 @@
-# !/usr/bin/python3
-# """ This module contains the Engine class. """
-# from models.engine.file_storage import FileStorage
-#
-# # """ Storage class for the engine. """
-# # storage = FileStorage()
-# # storage.reload()
-# #
+#!/usr/bin/python3
+'''
+    Package initializer
+'''
