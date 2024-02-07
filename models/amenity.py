ac957ea637c3ef66dd7309be4de71e6e8fd344c4
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-07 16:42:25 -0600

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Amenity class"""
+'''
+    Implementation of the Amenity class
+'''
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """class Amenity with the inheritance from Base"""
-    name = ''
-
-    def __init__(self, *args, **kwargs):
-        """initialize Amenity"""
-        super().__init__(*args, **kwargs)
+    '''
+        Implementation for the Amenities.
+    '''
+    name = ""
